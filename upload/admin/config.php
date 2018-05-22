1709492a0409acd9c7067b242133d060a35ba541
loctv <loc.tran@ascendcorp.com>
2018-05-22 22:36:37 +0700

    Deploy Cart to WebRoot

---
+++ upload/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/cart/admin/');
+define('HTTP_CATALOG', 'http://localhost/cart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/cart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/cart/');
+
+// DIR
+define('DIR_APPLICATION', '/Library/WebServer/Documents/cart/admin/');
+define('DIR_SYSTEM', '/Library/WebServer/Documents/cart/system/');
+define('DIR_IMAGE', '/Library/WebServer/Documents/cart/image/');
+define('DIR_STORAGE', '/Library/WebServer/storage/');
+define('DIR_CATALOG', '/Library/WebServer/Documents/cart/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Welcome1');
+define('DB_DATABASE', 'mysql');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
