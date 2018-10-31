59ac5b756d02c20cb7e72935bac7b437eba4f51d
loctv <loc.tran@ascendcorp.com>
2018-11-01 01:05:37 +0700

    Update to oc 3.0.2

--- admin/config.php
+++ admin/config.php
@@ -28,7 +28,7 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Welcome1');
-define('DB_DATABASE', 'mysql');
+define('DB_DATABASE', 'oc');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
